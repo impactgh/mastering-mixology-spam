3784851b76d8f113b4f7fcbcdb401921caf4f8a1
default <andrewr.jones@yahoo.com>
2026-03-06 19:45:58 +0000

    is it actually fixed?

--- src/main/java/com/masteringmixology/MixologyItemIDs.java
+++ src/main/java/com/masteringmixology/MixologyItemIDs.java
@@ -14,6 +14,8 @@ public class MixologyItemIDs {
 	public static final int LLL_UNFINISHED = 30017; // LipLack Liquor
 	public static final int MML_UNFINISHED = 30013; // Marley's MoonLight
 	public static final int MAL_UNFINISHED = 30020; // MixALot
+	public static final int MMM_UNFINISHED = 30014; // Mammoth-Might Mix
+	public static final int AAA_UNFINISHED = 30011; // Alco-AugmentAtor
 	
 	// Processed potions (after any station - same ID for all 3 stations)
 	public static final int ALA_PROCESSED = 30025;
@@ -24,6 +26,8 @@ public class MixologyItemIDs {
 	public static final int LLL_PROCESSED = 30027;
 	public static final int MML_PROCESSED = 30023;
 	public static final int MAL_PROCESSED = 30030;
+	public static final int MMM_PROCESSED = 30024;
+	public static final int AAA_PROCESSED = 30021;
 	
 	/**
 	 * Gets the recipe from an item ID.
@@ -38,6 +42,8 @@ public class MixologyItemIDs {
 			case 30017: case 30027: return PotionRecipe.LLL;
 			case 30013: case 30023: return PotionRecipe.MML;
 			case 30020: case 30030: return PotionRecipe.MAL;
+			case 30014: case 30024: return PotionRecipe.MMM;
+			case 30011: case 30021: return PotionRecipe.AAA;
 			default: return null;
 		}
 	}
@@ -48,7 +54,7 @@ public class MixologyItemIDs {
 	public static boolean isProcessed(int itemId) {
 		return itemId == 30025 || itemId == 30028 || itemId == 30022 || 
 		       itemId == 30026 || itemId == 30029 || itemId == 30027 || 
-		       itemId == 30023 || itemId == 30030;
+		       itemId == 30023 || itemId == 30030 || itemId == 30024 || itemId == 30021;
 	}
 	
 	/**
@@ -64,6 +70,8 @@ public class MixologyItemIDs {
 			case LLL: return 30017;
 			case MML: return 30013;
 			case MAL: return 30020;
+			case MMM: return 30014;
+			case AAA: return 30011;
 			default: return -1;
 		}
 	}
@@ -81,6 +89,8 @@ public class MixologyItemIDs {
 			case LLL: return 30027;
 			case MML: return 30023;
 			case MAL: return 30030;
+			case MMM: return 30024;
+			case AAA: return 30021;
 			default: return -1;
 		}
 	}
@@ -89,6 +99,6 @@ public class MixologyItemIDs {
 	 * Checks if an item ID is a mixology potion.
 	 */
 	public static boolean isMixologyPotion(int itemId) {
-		return (itemId >= 30012 && itemId <= 30020) || (itemId >= 30022 && itemId <= 30030);
+		return (itemId >= 30011 && itemId <= 30020) || (itemId >= 30021 && itemId <= 30030);
 	}
 }
